71b597330f1a7d60da46075c90941c10e36682d2
cassandra culkins <cassy.culkins@gmail.com>
2016-02-04 14:44:56 -0800

    working price output

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
-function Pizza(diameter, toppingVeg, toppingMeat, amountPizzas) {
+function Pizza(diameter, toppingVeg, toppingMeat) {
   this.diameter = diameter;
   this.toppingVeg = toppingVeg;
   this.toppingMeat = toppingMeat;
-  this.amountPizzas = amountPizzas;
+
+
 }
 
 Pizza.prototype.pizzaList = function () {
@@ -11,72 +12,81 @@ Pizza.prototype.pizzaList = function () {
 
 Pizza.prototype.pizzaPrice = function () {
   var price = 6;
+  var toppingMeatPrice = 0;
+  var diameterPrice = 0;
+  var toppingVegPrice = 0;
+  var totalPrice = [];
 
-  if(this.diameter === "size-fourteen"){
-    var diameterPrice = (price + 2);
-  } else if (this.diameter === "size-twelve"){
-    var diameterPrice = (price + 1);
+  if(this.diameter.length === "size-fourteen"){
+    diameterPrice = (price + 2);
+  } else if (this.diameter.length === "size-twelve"){
+    diameterPrice = (price + 1);
   } else {
-    var diameterPrice = price;
+    diameterPrice = price;
   };
 
-  if(this.toppingVeg === "four-veg"){
-    var toppingVegPrice = (diameterPrice + 1.5);
-  } else if (this.toppingVeg === "three-veg"){
-    var toppingVegPrice = (diameterPrice + 1);
-  } else if (this.toppingVeg === "two-veg"){
-    var toppingVegPrice = (diameterPrice + 0.5);
+  if(this.toppingVeg.length === 4){
+    toppingVegPrice = (diameterPrice + 1.5);
+  } else if (this.toppingVeg.length === 3){
+    toppingVegPrice = (diameterPrice + 1);
+  } else if (this.toppingVeg.length === 2){
+    toppingVegPrice = (diameterPrice + 0.5);
   } else {
-    var toppingVegPrice = diameterPrice;
+    toppingVegPrice = diameterPrice;
   };
 
-  if(this.toppingMeat === "four-meat"){
-    var toppingMeatPrice = (toppingVegPrice + 2.25);
-  } else if (this.toppingMeat === "three-meat"){
-    var toppingMeatPrice = (toppingVegPrice + 1.5);
-  } else if (this.toppingMeat === "two-meat"){
-    var toppingMeatPrice = (toppingVegPrice + 0.75);
+  if(this.toppingMeat.length === 4){
+    toppingMeatPrice = (toppingVegPrice + 2.25);
+  } else if (this.toppingMeat.length === 3){
+    toppingMeatPrice = (toppingVegPrice + 1.5);
+  } else if (this.toppingMeat.length === 2){
+    toppingMeatPrice = (toppingVegPrice + 0.75);
   } else {
-    var toppingMeatPrice = toppingVegPrice;
+    toppingMeatPrice = toppingVegPrice;
   };
-  return toppingMeatPrice;
+  totalPrice.push(toppingMeatPrice);
+
+  return parseFloat(totalPrice.join());
 };
 
-  if(this.amountPizzas === "two-pizzas" || this.amountPizzas === "three-pizzas" || this.amountPizzas === "four-pizzas" || this.amountPizzas === "five-pizzas") {
-    alert("Are you sure? How about we start with one for now.");
-  }
 
-$(document).ready(function() {
+  // if(this.amountPizzas === "two-pizzas" || this.amountPizzas === "three-pizzas" || this.amountPizzas === "four-pizzas" || this.amountPizzas === "five-pizzas") {
+  //   alert("Are you sure? How about we start with one for now.");
+  // }
 
-  $("form#form").submit(function(event) {
+  $(document).ready(function() {
 
-    var selectedAmount= $("select#select-amount").val();
-    var selectedSize = $("select#select-size").val();
-    var selectedVegNum = $("select#select-veg").val();
-    var selectedVeg= $("select#pick-veg").val();
-    var selectedMeatNum = $("select#select-meat").val();
-    var selectedMeat = $("select#pick-meat").val();
+    $("form#form").submit(function(event) {
 
-    $("span#final-amount").text(selectedAmount);
-    $("span#final-size").text(selectedSize);
-    $("span#final-veg").text(selectedVegNum);
-    $("span#final-veg-choice").text(selectedVeg);
-    $("span#final-meat").text(selectedMeatNum);
-    $("span#final-meat-choice").text(selectedMeat);
+      var selectedSize = [];
+      $("#pick-size :selected").each(function(i, item) {
+        selectedSize = $(item).val();
+      });
+      var selectedMeat = [];
+      $("#pick-meat :selected").each(function(i, item) {
+        selectedMeat[i] = $(item).val();
+      });
+      var selectedVeg = [];
+      $("#pick-veg :selected").each(function(i, item) {
+        selectedVeg[i] = $(item).val();
+      });
 
-    $("#pizzaSummary").show();
+      var newPizza = new Pizza(selectedSize, selectedVeg, selectedMeat);
+      var combinedPrice = newPizza.pizzaPrice();
 
 
-    // $("span#final-price").text(finalPrice.pizzaPrice());
-    // $("#priceSummary").show();
+      $("span#final-size").text(selectedSize);
+      $("span#final-veg-choice").text(selectedVeg);
+      $("span#final-meat-choice").text(selectedMeat);
+      $("span.totalPrice").text(combinedPrice);
 
-    var selectedAmount= $(this).find("select#select-amount").val();
-    var selectedSize = $(this).find("select#select-size").val();
-    var selectedVegNum = $(this).find("select#select-veg").val();
-    var selectedVeg= $(this).find("select#pick-veg").val();
-    var selectedMeatNum = $(this).find("select#select-meat").val();
-    var selectedMeat = $(this).find("select#pick-meat").val();
+      $("#pizzaSummary").show();
 
-    event.preventDefault();
+
+      // $("span#totalPrice").text(finalPrice.pizzaPrice());
+      // $("#priceSummary").show();
+
+
+      event.preventDefault();
+    });
   });
-});
